bd7857373bf0fe0d50deb6c0b7a8fae4cf802bfc
runner <runner@Mac-1706862491632.local>
2024-02-02 09:01:00 +0000

    Released new version 3.6.1

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 
 import PackageDescription
 
-let version = "3.6.0"
-let checksum = "8f4a4370eca02b64ad587b030a4d0b48f9053439dd0fc234250265c4229ec122"
+let version = "3.6.1"
+let checksum = "f7c8f7d27679b9c341fd3a78b0804fbccb8462f5c8576097fb599df9ae7d86fa"
 
 let package = Package(
 	name: "NevisMobileAuthentication",
