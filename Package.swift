9abfdd4844ac136165cbdeea29cf5fd3ea41b3e6
runner2 <runner@sharednevis002.local>
2025-03-05 11:02:09 +0100

    Released new version 3.9.0

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 
 import PackageDescription
 
-let version = "3.8.2"
-let checksum = "e800453e845852419e704d2b28ecd90747aeabae003842dd439c6b400d94b05d"
+let version = "3.9.0"
+let checksum = "70f2372f9db5f9c70695428c68760155a6b3dc299bb705ddedb692a0445c87e1"
 
 let package = Package(
 	name: "NevisMobileAuthentication",
