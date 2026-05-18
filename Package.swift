2dfe8b912b552b92d017874ba9058b8608db2332
runner2 <runner@sharednevis002.local>
2026-05-18 09:13:37 +0200

    Released new version 4.4.0

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 
 import PackageDescription
 
-let version = "4.3.2"
-let checksum = "72374faf420f5de77865ee73e871afa870281cd8447476e8f21282ac75259e2a"
-let checksumDebug = "3fc3bd1c94071d097674b280d40c8f92fd8b8aafd9bcda4ecd1ec38451ec7749"
+let version = "4.4.0"
+let checksum = "9639eb1c376b10ab6e18508e59a511f0bbe775db3e487bf87b8eb28fdcbdbf58"
+let checksumDebug = "b9710d6f286b63b02d84fe1b1033cb1f5506dee505b1872897e206c7768ab933"
 
 let package = Package(
 	name: "NevisMobileAuthentication",
